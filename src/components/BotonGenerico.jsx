daa4c487a18e74247b258a123ae28d445edd6007
Mateosacco99 <mateosacco99@gmail.com>
2026-04-21 14:06:50 -0300

    Feat: Implementando componente BotonGenerico

---
+++ src/components/BotonGenerico.jsx
@@ -0,0 +1,27 @@
+import styles from '../styles/botonGenerico.module.scss';
+
+export function BotonGenerico ({
+    tipo = 'primario',
+    children,
+    onClick,
+    disabled = false,
+    className='',
+    ...props
+}) {
+    const tiposDeBoton = [
+        'accion',
+        'accionSecundario',
+    ];
+    const tipoValido = tiposDeBoton.includes(tipo) ? tipo : 'primario';
+
+    return (
+        <button
+            className={`${styles.boton} ${styles[`boton${tipoValido.charAt(0).toUpperCase() + tipoValido.slice(1)}`]} ${className}`}
+            onClick={onClick}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
